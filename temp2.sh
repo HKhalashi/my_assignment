3074ceb50112d33b0e31f18bc59036d565455a35
HKhalashi <mohammadhosseinkhalashi@gmail.com>
2021-07-14 01:29:24 +0300

    temp4.sh has been deleted

--- temp2.sh
+++
@@ -1,8 +0,0 @@
-# file : temp2.sh
-example_func(){
-local result=$( expr $2 - $1 )
-return $result
-}
-
-local res=example_func "20" "10"
-echo "the res is $res"
--- temp3.sh
+++
@@ -1,9 +0,0 @@
-# file : temp3.sh
-echo "please enter a input:"
-read res
-while [[ $res != 5 ]]
- do 
-  echo "try again:"
-  read res
- done
-echo "program ended"
